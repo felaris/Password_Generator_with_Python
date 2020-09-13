de920e0afa61035f412c26fe549e4ba4b9ab5b5b
Felix Awortwe <felixawortwe14@gmail.com>
2020-09-13 01:52:21 -0700

    first commit

---
+++ password_generator.py
@@ -0,0 +1,16 @@
+import string
+import random
+
+def generate():
+    s1= string.ascii_uppercase
+
+    s2= string.ascii_lowercase
+
+    s3=string.digits
+
+    s4=string.punctuation
+
+    passlen=input("Enter the Password lenght\n")
+
+    passlen = int(passlen)
+    
